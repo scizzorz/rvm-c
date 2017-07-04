3a4515eb82d3a092ea1265252929734e7d36d3f3
John Weachock <jweachock@gmail.com>
2017-07-04 18:06:14 -0400

    main: Add box_print and op_print

--- main.c
+++ main.c
@@ -45,20 +45,53 @@ typedef struct R_vm {
   R_box *stack;
 } R_vm;
 
-R_vm *vm_new(FILE *fp) {
+void box_print(R_box *val) {
+  switch(val->type) {
+    case TYPE_NULL:
+      printf("null\n");
+      break;
+    case TYPE_INT:
+      printf("%ld\n", val->data.si);
+      break;
+    case TYPE_FLOAT:
+      printf("%f\n", val->data.f);
+      break;
+    case TYPE_BOOL:
+      printf(val->data.si != 0 ? "true\n" : "false\n");
+      break;
+    case TYPE_STR:
+      printf("%s\n", val->data.s);
+      break;
+    case TYPE_TABLE:
+      printf("table 0x%08lx\n", (unsigned long)val->data.p);
+      break;
+    case TYPE_FUNC:
+      printf("func 0x%08lx\n", (unsigned long)val->data.p);
+      break;
+    case TYPE_CDATA:
+      printf("cdata 0x%08lx\n", (unsigned long)val->data.p);
+      break;
+    default:
+      printf("unknown\n");
+  }
+}
+
+void op_print(R_op *instr) {
+  printf("Instr<%d: %d %d %d>\n", instr->op, instr->args[0], instr->args[1], instr->args[2]);
+}
+
+R_vm *vm_load(FILE *fp) {
   int rv;
 
   R_vm *this = GC_malloc(sizeof(R_vm));
 
   rv = fread(&this->num_consts, sizeof(int), 1, fp);
-
   if(rv != 1) {
     fprintf(stderr, "Unable to read num_consts\n");
     return NULL;
   }
 
   rv = fread(&this->num_instrs, sizeof(int), 1, fp);
-
   if(rv != 1) {
     fprintf(stderr, "Unable to read num_instrs\n");
     return NULL;
@@ -100,7 +133,7 @@ int main(int argv, char **argc) {
     return 1;
   }
 
-  R_vm *this = vm_new(fp);
+  R_vm *this = vm_load(fp);
 
   return 0;
 }
