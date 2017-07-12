d70f2b224d1f4f2c5f5fd4787747626fb7f27a24
John Weachock <jweachock@gmail.com>
2017-07-12 19:43:21 -0400

    Move Python packaging

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='rvmpy',
+      version='0.1',
+      description='A bytecode generator for Rain.',
+      url='https://github.com/scizzorz/rvm-c',
+      author='John Weachock',
+      author_email='jweachock@gmail.com',
+      license='MIT',
+      packages=['rvmpy'],
+      install_requires=[],
+      zip_safe=False)
