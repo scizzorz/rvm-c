88472dbb4967a5d5bf8b736739189f434d106a5e
John Weachock <jweachock@gmail.com>
2017-07-12 20:03:28 -0400

    vm: Add intrinsic load

--- vm.c
+++ vm.c
@@ -1,6 +1,23 @@
 #include "rain.h"
 
 #include <limits.h>
+#define __USE_GNU
+#include <dlfcn.h>
+
+void R_intrinsic_load(R_vm *vm) {
+  R_box name = vm_pop(vm);
+  R_box lib = vm_top(vm);
+  R_box *top = &vm->stack[vm->stack_ptr - 1];
+
+  if(R_TYPE_IS(&name, STR) && R_TYPE_IS(&lib, STR)) {
+    void *handle = dlopen(lib.str, RTLD_LAZY | RTLD_GLOBAL);
+    void *func = dlsym(handle, name.str);
+    R_set_cfunc(top, func);
+    return;
+  }
+
+  R_set_null(top);
+}
 
 R_vm *vm_new() {
   GC_init();
@@ -45,7 +62,16 @@ bool vm_import(R_vm *this, const char *fname) {
     return false;
   }
 
-  vm_call(this, next_instr, NULL);
+  R_box builtins;
+  R_box key;
+  R_box val;
+
+  R_set_table(&builtins);
+  R_set_str(&key, "load");
+  R_set_cfunc(&val, R_intrinsic_load);
+  R_table_set(&builtins, &key, &val);
+
+  vm_call(this, next_instr, &builtins);
   return true;
 }
 
